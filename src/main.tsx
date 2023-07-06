ea5a4e27666fa6a1d992600668f8ee604c01f512
HammadYousaf01 <hammad.yousaf@arbisoft.com>
2023-07-06 13:04:26 +0500

    Migrated project to TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import "./main.css";
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
